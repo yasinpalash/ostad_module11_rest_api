2e4847986c37772393cc8bf9f6357538d4da1884
yasin <yasinmiapolash@gmail.com>
2023-11-04 21:40:50 +0600

    update all

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,127 @@
 import 'package:flutter/material.dart';
-import 'package:moduel/screens/product_list_screen.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
-  runApp(const CrudApp());
+  runApp(MyApp());
 }
 
-class CrudApp extends StatelessWidget {
-  const CrudApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: ProductListScreen(),
-
+    return MaterialApp(
+      home: PhotoListScreen(),
     );
   }
 }
 
+class PhotoListScreen extends StatefulWidget {
+  @override
+  _PhotoListScreenState createState() => _PhotoListScreenState();
+}
+
+class _PhotoListScreenState extends State<PhotoListScreen> {
+  List<Photo> photos = [];
+  bool isLoading = true;
+  String error = '';
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPhotos();
+  }
+
+  Future<void> fetchPhotos() async {
+    try {
+      final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
+
+      if (response.statusCode == 200) {
+        final List<dynamic> data = json.decode(response.body);
+        setState(() {
+          photos = data.map((json) => Photo.fromJson(json)).toList();
+          isLoading = false;
+        });
+      } else {
+        setState(() {
+          error = 'Failed to fetch photos';
+          isLoading = false;
+        });
+      }
+    } catch (e) {
+      setState(() {
+        error = 'Failed to fetch photos';
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Photo Gallery App'),
+      ),
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : error.isNotEmpty
+          ? Center(child: Text(error))
+          : ListView.builder(
+        itemCount: photos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(photos[index].title),
+            leading: Image.network(photos[index].thumbnailUrl),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => PhotoDetailScreen(photo: photos[index]),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class PhotoDetailScreen extends StatelessWidget {
+  final Photo photo;
+
+  PhotoDetailScreen({required this.photo});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Photo Details'),
+      ),
+      body: Column(
+        children: [
+          Image.network(photo.url),
+          Text('ID: ${photo.id}'),
+          Text('Title: ${photo.title}'),
+        ],
+      ),
+    );
+  }
+}
+
+class Photo {
+  final int id;
+  final String title;
+  final String url;
+  final String thumbnailUrl;
+
+  Photo({required this.id, required this.title, required this.url, required this.thumbnailUrl});
+
+  factory Photo.fromJson(Map<String, dynamic> json) {
+    return Photo(
+      id: json['id'],
+      title: json['title'],
+      url: json['url'],
+      thumbnailUrl: json['thumbnailUrl'],
+    );
+  }
+}
